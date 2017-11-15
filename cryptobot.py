78781f90f227f2f6bd4e07e800c296569af2cd29
Frederik <frederik.bolding@gmail.com>
2017-11-15 23:13:00 +0100

    Initial commit with basic ticker

---
+++ cryptobot.py
@@ -0,0 +1,54 @@
+import configparser
+import requests
+
+import discord
+from discord.ext import commands
+import random
+
+def getTicker(crypto, currency):
+    if currency == "":
+      currency = "USD"
+    crypto_list = requests.get("https://api.coinmarketcap.com/v1/ticker/?convert=" + currency.upper()).json()
+    crypto_dict = {}
+    for c in crypto_list:
+        crypto_dict[c["symbol"].upper()] = c
+    return crypto_dict[crypto.upper()]
+
+
+config = configparser.ConfigParser()
+print("Reading config..")
+config.read('config.ini')
+
+description = '''A bot that helps you keep track of the latest in crypto.'''
+bot = commands.Bot(command_prefix='.', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.group(pass_context=True)
+async def crypto(ctx):
+    if ctx.invoked_subcommand is None:
+        await bot.say('Oops, thats an invalid command!')
+
+@crypto.command()
+async def ticker(*, args: str):
+    """Posts a ticker message with details about the crypto"""
+    a = args.split(" ")
+    currency = "USD"
+    if len(a) > 1:
+        currency = a[1]
+        info = getTicker(a[0],a[1])
+    elif len(a) == 1:
+        info = getTicker(a[0],currency)
+    await bot.say(info["name"] + " (" + info["symbol"] + ") is currently priced at " + info["price_" + currency.lower()] + " " + currency.upper() + ".")
+
+@crypto.command()
+async def source():
+    """Posts a link to the bot GitHub page."""
+    await bot.say("The source can be found here: https://github.com/FrederikBolding/CryptoBot")
+
+bot.run(config["DEFAULT"]["token"])
