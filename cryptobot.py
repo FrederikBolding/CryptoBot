c858c8d011afb0a21d1b22586e589eb91215d2f5
Frederik <frederik.bolding@gmail.com>
2017-11-16 15:10:10 +0100

    Added custom currency conversion and 'cached' results

--- cryptobot.py
+++ cryptobot.py
@@ -1,20 +1,33 @@
 import configparser
 import os
+import datetime
+from datetime import datetime
+from datetime import timedelta
 import requests
 
 import discord
 from discord.ext import commands
-import random
+from forex_python.converter import CurrencyRates
 
 
 def getTicker(crypto, currency):
-    if currency == "":
-        currency = "USD"
-    crypto_list = requests.get(
-        "https://api.coinmarketcap.com/v1/ticker/?convert=" + currency.upper()).json()
-    crypto_dict = {}
-    for c in crypto_list:
-        crypto_dict[c["symbol"].upper()] = c
+    now = datetime.now()
+    global last_crypto_update
+    diff = now - last_crypto_update
+    minutes = diff / timedelta(minutes=1)
+    if minutes > minutesBetweenUpdates:
+        print("Information was too old, getting new info..")
+        if currency == "":
+            currency = "USD"
+        crypto_list = requests.get(
+            "https://api.coinmarketcap.com/v1/ticker/").json()
+        last_crypto_update = datetime.now()
+        for c in crypto_list:
+            prices = {"USD": c["price_usd"]}
+            for rate in rates:
+                prices[rate] = float(c["price_usd"]) * rates[rate]
+            c["prices"] = prices
+            crypto_dict[c["symbol"].upper()] = c
     return crypto_dict[crypto.upper()]
 
 
@@ -34,8 +47,15 @@ if config["DEFAULT"]:
 elif os.environ.get("token", None):
     token = os.environ["token"]
 else:
-    print("No config file found :(")
+    print("No config/token found :(")
     raise ValueError("Missing config/token")
+
+crypto_dict = {}
+currency_rates = CurrencyRates()
+print("Getting currency rates..")
+rates = currency_rates.get_rates("USD")
+minutesBetweenUpdates = 5
+last_crypto_update = datetime.min
 
 
 description = '''A bot that helps you keep track of the latest in crypto.'''
@@ -66,7 +86,7 @@ async def ticker(*, args: str):
         info = getTicker(a[0], a[1])
     elif len(a) == 1:
         info = getTicker(a[0], currency)
-    await bot.say(info["name"] + " (" + info["symbol"] + ") is currently priced at " + info["price_" + currency.lower()] + " " + currency.upper() + ".\n\nThe price has changed with: " + formatPercentage(info["percent_change_1h"]) + " in the last hour, " + formatPercentage(info["percent_change_24h"]) + " in the last 24 hours, and " + formatPercentage(info["percent_change_7d"]) + " in the last 7 days.")
+    await bot.say(info["name"] + " (" + info["symbol"] + ") is currently priced at " + str(info["prices"][currency.upper()]) + " " + currency.upper() + ".\n\nThe price has changed with: " + formatPercentage(info["percent_change_1h"]) + " in the last hour, " + formatPercentage(info["percent_change_24h"]) + " in the last 24 hours, and " + formatPercentage(info["percent_change_7d"]) + " in the last 7 days.\n\nData from: <https://coinmarketcap.com/> - Last Updated: " + datetime.fromtimestamp(int(info["last_updated"])).strftime('%Y-%m-%d %H:%M:%S'))
 
 
 @crypto.command()
