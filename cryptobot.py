8e52714dcb6e7dffcb86d2ee74131c039425e18c
Frederik <frederik.bolding@gmail.com>
2017-11-16 10:30:09 +0100

    Fixed formatting

--- cryptobot.py
+++ cryptobot.py
@@ -6,10 +6,12 @@ import discord
 from discord.ext import commands
 import random
 
+
 def getTicker(crypto, currency):
     if currency == "":
-      currency = "USD"
-    crypto_list = requests.get("https://api.coinmarketcap.com/v1/ticker/?convert=" + currency.upper()).json()
+        currency = "USD"
+    crypto_list = requests.get(
+        "https://api.coinmarketcap.com/v1/ticker/?convert=" + currency.upper()).json()
     crypto_dict = {}
     for c in crypto_list:
         crypto_dict[c["symbol"].upper()] = c
@@ -22,7 +24,7 @@ config.read('config.ini')
 
 if config["DEFAULT"]:
     token = config["DEFAULT"]["token"]
-elif os.environ.get("token",None):
+elif os.environ.get("token", None):
     token = os.environ["token"]
 else:
     print("No config file found :(")
@@ -32,6 +34,7 @@ else:
 description = '''A bot that helps you keep track of the latest in crypto.'''
 bot = commands.Bot(command_prefix='.', description=description)
 
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -39,10 +42,12 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+
 @bot.group(pass_context=True)
 async def crypto(ctx):
     if ctx.invoked_subcommand is None:
         await bot.say('Oops, thats an invalid command!')
+
 
 @crypto.command()
 async def ticker(*, args: str):
@@ -51,10 +56,11 @@ async def ticker(*, args: str):
     currency = "USD"
     if len(a) > 1:
         currency = a[1]
-        info = getTicker(a[0],a[1])
+        info = getTicker(a[0], a[1])
     elif len(a) == 1:
-        info = getTicker(a[0],currency)
+        info = getTicker(a[0], currency)
     await bot.say(info["name"] + " (" + info["symbol"] + ") is currently priced at " + info["price_" + currency.lower()] + " " + currency.upper() + ".")
+
 
 @crypto.command()
 async def source():
